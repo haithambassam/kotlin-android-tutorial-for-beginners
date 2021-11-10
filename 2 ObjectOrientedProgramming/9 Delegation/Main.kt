ba2b3dd0f8f7af28b0a081e2ddd05632a69efc78
haithambassam <73366895+haithambassam@users.noreply.github.com>
2021-11-10 22:10:13 +0400

    Update Main.kt

--- 2 ObjectOrientedProgramming/9 Delegation/Main.kt
+++ 2 ObjectOrientedProgramming/9 Delegation/Main.kt
@@ -10,10 +10,29 @@ class FilePlayer(private val file: String) : Player {
     }
 }
 
+
+// This is delegation, Kotlin helps you to achieve this without writing the boilerplate code that exists in the second example.
 class MediaFile(
         private val downloader: Downloader,
         private val player: Player
 ) : Downloader by downloader, Player by player
+
+
+
+// This is delegation, this contains boilderplate code, both code works.
+class MediaFile(
+        private val downloader: Downloader,
+        private val player: Player
+) : Downloader , Player{
+    
+    override fun download() {
+        downloader.downlaod()
+    }
+    
+    override fun play() {
+        player.play()
+    }
+}
 
 
 fun main() {
